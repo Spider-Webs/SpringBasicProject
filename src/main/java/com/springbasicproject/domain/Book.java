a0ed90e75a6e5b57850ea351344754413fce61ae
Spider-Webs <dyko615@naver.com>
2022-08-26 13:49:25 +0900

    book 도메인

---
+++ src/main/java/com/springbasicproject/domain/Book.java
@@ -0,0 +1,41 @@
+package com.springbasicproject.domain;
+
+public class Book {
+
+    private Long idx;
+    private String subject;
+    private String category;
+    private int price;
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
